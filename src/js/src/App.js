df89d8a4e4615ee722990b7a01713ec749c6938a
simonjv01 <sjvargas01@gmail.com>
2020-08-22 09:12:08 -0400

    updates

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { getAllStudents } from './client';   
 
-function App() {
-  return (
-    <h1>Hello World Spring Boot & React</h1>
-  );
+class App extends Component {
+
+  state = {
+    students: []
+  }
+
+  componentDidMount () {
+    this.fetchStudents();
+
+  }
+
+  fetchStudents = () => {
+    getAllStudents()
+    .then(res => res.json()
+    .then(students => {
+      console.log(students);
+      this.setState({
+        students
+      });
+    }));
+  }
+  
+  render() {
+    
+    const { students } = this.state;
+
+    if (students && students.length) {
+
+
+    return students.map((student, index) => {
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+            <p>{student.email}</p>
+          </div>
+        )
+      })
+    }
+  
+     return <h1>No Students found</h1>
+    
+  }
+   
 }
 
 export default App;

0a81e7a7e16eda0f3a29165e22e84756eba3fefe
simonjv01 <sjvargas01@gmail.com>
2020-08-23 23:57:06 -0400

    updates to JS files

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getAllStudents } from './client';   
+import { getAllStudents } from './client'; 
+import {
+  Table
+}  from 'antd';
 
 class App extends Component {
 
@@ -30,18 +33,34 @@ class App extends Component {
 
     if (students && students.length) {
 
-
-    return students.map((student, index) => {
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-            <p>{student.email}</p>
-          </div>
-        )
-      })
+      const columns = [
+        {
+          title: 'Student Id',
+          dataIndex: 'studentId',
+          key: 'studentId'
+        },
+        {
+          title: 'First Name',
+          dataIndex: 'firstName',
+          key: 'firstName'
+        },
+        {
+        title: 'Last Name',
+        dataIndex: 'lastName',
+        key: 'lastName'
+        },
+        {
+        title: 'Email',
+        dataIndex: 'email',
+        key: 'email'
+      },
+      {
+        title: 'Gender',
+        dataIndex: 'gender',
+        key: 'gender'
+      }
+      ];
+      return <Table dataSource={students} columns={columns} rowKey='studentId' />;
     }
   
      return <h1>No Students found</h1>
---
+++ src/js/src/Container.js
@@ -0,0 +1,11 @@
+export const Container = props => (
+    <div style={{width: '1400px', margin: '0 auto'}}>
+
+        {props.children}
+
+
+
+    </div>
+)
+
+export default Container;

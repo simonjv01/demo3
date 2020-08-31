329ddf02af4d7cee6f2a208aff4d551334d5358c
simonjv01 <sjvargas01@gmail.com>
2020-08-31 18:31:23 -0400

    updated dependencies

--- src/main/java/com/simonvargas/demo/DemoApplication.java
+++ src/main/java/com/simonvargas/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.simonvargas.demo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
 
 }
